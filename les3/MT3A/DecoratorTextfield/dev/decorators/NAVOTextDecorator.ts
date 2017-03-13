d9efdb197b016ac00eb5d7ac8548cf4ee19f72ff
bpikaar <bpikaar@users.noreply.github.com>
2017-03-13 12:59:22 +0100

    Jens van de Graaf

---
+++ les3/MT3A/DecoratorTextfield/dev/decorators/NAVOTextDecorator.ts
@@ -0,0 +1,62 @@
+/*
+ * Jens van de Graaf
+ *
+ */
+ 
+/// <reference path="TextDecorator.ts" />
+
+class NAVOTextDecorator extends TextDecorator {
+    public txt : Txt;
+
+    constructor(txt : Txt) {
+        super();
+        this.txt = txt;
+    }
+    
+    public getText() : string {
+        let text : string = this.txt.getText();
+        text = text.toUpperCase();
+
+        let navoSpelling = {
+            "A" : "Alpha", 
+            "B" : "Bravo", 
+            "C" : "Charlie", 
+            "D" : "Delta",  
+            "E" : "Echo", 
+            "F" : "Foxtrot", 
+            "G" : "Golf", 
+            "H" : "Hotel",         
+            "I" : "India", 
+            "J" : "Juliet", 
+            "K" : "Kilo", 
+            "L" : "Lima", 
+            "M" : "Mike", 
+            "N" : "November", 
+            "O" : "Oscar", 
+            "P" : "Papa", 
+            "Q" : "Quebec", 
+            "R" : "Romeo", 
+            "S" : "Sierra", 
+            "T" : "Tango", 
+            "U" : "Uniform", 
+            "V" : "Victor", 
+            "W" : "Whiskey", 
+            "X" : "Xray", 
+            "Y" : "Yankee", 
+            "Z" : "Zulu"
+        };
+
+        let newText : string = "";
+
+        for(let i = 0; i < text.length; i++) {
+            if(!navoSpelling.hasOwnProperty(text[i])) {
+                newText += text[i];
+                continue;
+            }
+
+            newText += text[i].replace(text[i], navoSpelling[text[i]]);
+        }
+
+        return newText;
+    }
+}
